b724d6ef3eb8dc7867528d4698ec0deab14762b3
ffrancis <ffrancis@qti.qualcomm.com>
2026-03-24 22:15:54 +0530

    Enable Kaanapali and Pakala target compilation
    Add build scripts to enable Kaanapali and Pakala target compilation.[1]
    
    [1] https://github.com/AudioReach/audioreach-workflows/pull/69
    
    Signed-off-by: ffrancis <ffrancis@qti.qualcomm.com>

---
+++ ci/pkl_build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+set -ex
+
+PREBUILD_SCRIPT_PATH="${PREBUILD_SCRIPT:-$(dirname "${BASH_SOURCE[0]}")/pre_build.sh}"
+source "$PREBUILD_SCRIPT_PATH"
+
+
+# load build args from file if environment variable is not set
+if [ -z "${BUILD_ARGS}" ]; then
+    BUILD_OPTIONS_FILE="${GITHUB_WORKSPACE}/ci/build_options.txt"
+    BUILD_ARGS="$(sed -E 's/#.*$//' "$BUILD_OPTIONS_FILE" | sed '/^[[:space:]]*$/d' | tr '\n' ' ')"
+fi
+
+echo "Running build script..."
+# Build/Compile audioreach-pal
+source ${GITHUB_WORKSPACE}/install/environment-setup-armv8-7a-qcom-linux
+
+# make sure we are in the right directory
+cd ${GITHUB_WORKSPACE}
+#install headers
+cd inc
+autoreconf -Wcross --verbose --install --force --exclude=autopoint
+autoconf --force
+./configure ${BUILD_ARGS}
+make DESTDIR=$SDKTARGETSYSROOT  install
+
+cd ..
+
+autoreconf -Wcross --verbose --install --force --exclude=autopoint
+autoconf --force
+
+# Run the configure script with the specified arguments
+./configure ${BUILD_ARGS} --with-glib --with-syslog
+# make
+make DESTDIR=${GITHUB_WORKSPACE}/build install
